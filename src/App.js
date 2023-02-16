2484538175522779bf2085be4d1b5e2256e416d6
Danny Battison <captain@danny.gg>
2023-02-16 19:06:06 +0000

    Add animations

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 import Questions from "./QuestionsData";
 import {useState} from "react";
 import Question from "./Question";
+import { motion } from "framer-motion";
 
 function App() {
 	const [question, setQuestion] = useState(Questions.YouPlayAs);
 
   return (
     <div className="App">
-			<Question question={question} setQuestion={setQuestion} />
+			<motion.div
+				key={question.Text}
+				animate={{
+					scale: [0.2, 1],
+					opacity: ["0%", "100%"],
+				}}
+			>
+				<Question question={question} setQuestion={setQuestion} />
+			</motion.div>
     </div>
   );
 }
