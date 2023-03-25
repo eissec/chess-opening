75c87293bb904254b92fdefb4e0dbd8283c626c9
eissec <103186287+eissec@users.noreply.github.com>
2023-03-25 16:33:23 +0100

    styling and added some divs to use flexbox

--- src/Question.js
+++ src/Question.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React from "react";
 import Questions from "./QuestionsData";
 
-export default ({question, setQuestion}) => {
-	return <>
-		<h1>{ question.Text }</h1>
-		{ question.Answers && question.Answers.map(answer =>
-			<button onClick={() => setQuestion(Questions[answer.LeadsTo])}>
-				{ answer.Text }
-			</button>
-		) }
-	</>;
-}
+export default ({ question, setQuestion }) => {
+  return (
+    <>
+      <h1>{question.Text}</h1>
+      <div className="buttonBlock">
+        {question.Answers &&
+          question.Answers.map((answer) => (
+            <button onClick={() => setQuestion(Questions[answer.LeadsTo])}>
+              {answer.Text}
+            </button>
+          ))}
+      </div>
+    </>
+  );
+};
